76e501d759bc09710df3bf53db87c063cefeb7df
github username <github email address>
2019-06-13 11:57:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function sum(a,b){
+  return a+b;
+}
+
+class Polygon{
+  constructor(sides){
+    this.sides = sides;
+  }
+
+  get countSides(){
+    return this.sides.length
+  }
+
+  get perimeter(){
+    return this.sides.reduce(sum)
+  }
+}
+
+class Triangle extends Polygon{
+  get isValid(){
+    return this.countSides === 3 &&
+           this.sides[0] + this.sides[1] > this.sides[2] &&
+           this.sides[1] + this.sides[2] > this.sides[0] &&
+           this.sides[0] + this.sides[2] > this.sides[1]
+  }
+}
+
+class Square extends Polygon{
+
+  get area(){
+    return this.sides[0] ** 2
+  }
+
+  get isValid(){
+    return this.countSides === 4 &&
+           this.sides[0] === this.sides[1] &&
+           this.sides[1] === this.sides[2] &&
+           this.sides[2] === this.sides[3]
+
+  }
+}
